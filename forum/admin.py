7dae31bba6c05c01ab8e03897ea4bd7669760242
gabriellyrcarneiro <gabriellyrcarneiros@gmail.com>
2026-05-14 00:04:32 -0300

    feat: configurar painel admin do forum

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Category, Question, Answer
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ["name"]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "category", "is_solved", "created_at"]
+    list_filter = ["category", "is_solved", "created_at"]
+    search_fields = ["title", "body", "author__username"]
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ["question", "author", "is_best", "created_at"]
+    list_filter = ["is_best", "created_at"]
+    search_fields = ["body", "author__username", "question__title"]
